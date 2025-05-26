9dce8897cba27ad3c00094c4d417cdde1859f008
CaseyBartman <caseywilliambartman@gmail.com>
2025-05-26 15:07:19 -0400

    Added basic event handler extending existing event emitter using util

--- 02-Events/app.js
+++ 02-Events/app.js
@@ -1,12 +1,35 @@
-var Emitter = require('./emitter');
+// var Emitter = require('./emitter'); //using the custom even emitter we made
+var EventEmitter = require('events');
+var eventConfig = require('./config');
+var util = require('util');
 
-var emtr = new Emitter();
-emtr.on('greet', function() {
-    console.log('Someone said hello!');
+// var emtr = new Emitter();
+// emtr.on(eventConfig.GREET, function() {
+//     console.log('Someone said hello!');
+// });
+
+// emtr.on(eventConfig.GREET, function() {
+//     console.log('A greeting occured!');
+// });
+
+// emtr.emit(eventConfig.GREET);s
+
+function Greetr() {
+    this.greeting = 'Hello World!';
+}
+
+//Here, we are making it so that any objects created from Greetr also have access to anything in EventEmmitter!
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) { 
+    console.log(this.greeting + ': ' + data);
+    this.emit('greet', data);
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+    console.log('Someone greeted!: ' + data);
 });
 
-emtr.on('greet', function() {
-    console.log('A greeting occured!');
-});
-
-emtr.emit('greet');
+greeter1.greet('Casey!');
--- 02-Events/emitter.js
+++ 02-Events/emitter.js
@@ -1,22 +1,22 @@
-function Emitter() {
-    this.events = {};
-    //prop: [];
-}
+// function Emitter() {
+//     this.events = {};
+//     //prop: [];
+// }
 
-//Creating an 'on' function that is given a type and listener
-//Creates a new property on the object, makes sure its an array, then adds functions (the listener) to each one
-Emitter.prototype.on = function(type, listener) {
-    this.events[type] = this.events[type] || [];
-    this.events[type].push(listener);
-}
+// //Creating an 'on' function that is given a type and listener
+// //Creates a new property on the object, makes sure its an array, then adds functions (the listener) to each one
+// Emitter.prototype.on = function(type, listener) {
+//     this.events[type] = this.events[type] || [];
+//     this.events[type].push(listener);
+// }
 
-//Emitting an event
-Emitter.prototype.emit = function(type) {
-    if(this.events[type]) {
-        this.events[type].forEach(function(listener) {
-            listener();
-        }
-    )}
-}
+// //Emitting an event
+// Emitter.prototype.emit = function(type) {
+//     if(this.events[type]) {
+//         this.events[type].forEach(function(listener) {
+//             listener();
+//         }
+//     )}
+// }
 
-module.exports = Emitter;
+// module.exports = Emitter;
