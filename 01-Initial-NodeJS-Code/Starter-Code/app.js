c9b5d141f8687106c73d86061e4dc1d86669fba8
CaseyBartman <caseywilliambartman@gmail.com>
2025-05-26 13:09:22 -0400

    Added and deleted loads of notation practice

---
+++ 01-Initial-NodeJS-Code/Starter-Code/app.js
@@ -0,0 +1,4 @@
+// var greet = require('./greet.js');
+//greet();
+import greet from './greet.js'
+greet();
---
+++ 01-Initial-NodeJS-Code/Starter-Code/greet.js
@@ -0,0 +1,11 @@
+export default function greet() {
+    console.log('Hello');
+};
+// (function () {
+//     var firstname = "Casey";
+//     console.log(firstname);
+// })(); 
+
+// var firstname = "John";
+// console.log(firstname);
+
