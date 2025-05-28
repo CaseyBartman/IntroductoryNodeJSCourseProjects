6dfeff477d8e9df0d1899d8a03a038dddd995dfc
CaseyBartman <caseywilliambartman@gmail.com>
2025-05-28 18:40:42 -0400

    Added simple parsing

--- 06-NPM-and-Express/app.js
+++ 06-NPM-and-Express/app.js
@@ -1,6 +1,7 @@
 // var moment = require('moment');
 // console.log(moment().format("ddd, hA"));
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000; 
@@ -10,6 +11,9 @@ var port = process.env.PORT || 3000;
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+var jsonParser = bodyParser.json();
 
 app.use('/', function(req, res, next) {
     console.log('Request Url: ' + req.url);
@@ -23,7 +27,18 @@ app.get('/', function(req, res) {
 
 app.get('/person/:id', function(req, res) {
     //res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet></head><body><h1>Person: ' + req.params.id + 'World!</h1></body></html>');
-        res.render('person', {ID: req.params.id});
+        res.render('person', {ID: req.params.id, Qstr: req.query.qstr});
+})
+app.post('/person', urlencodedParser, function(req, res) {
+    res.send('Thank You!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+})
+
+app.post('/personjson', jsonParser, function(req, res) {
+    res.send('Thank You for the JSON Data!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
 })
 
 app.get('/api', function(req, res) {
